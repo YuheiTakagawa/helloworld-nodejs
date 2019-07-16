c5a2c03aaba42a830369ff87ee2b318240751686
YuheiTakagawa <roto11fon@gmail.com>
2019-07-16 16:38:19 +0900

    refactor: separate function with router

--- hello/index.js
+++ hello/index.js
@@ -2,23 +2,29 @@ const express = require('express')
 const router = express.Router()
 
 router
-    .get('/', (req, res) => {
-        res.send('Simple REST API')
-    })
-    .get('/hello', (req, res) => {
-        let name = ""
-        if (req.query.name) {
-            name = req.query.name
-        }
-        res.send('Hello World, ' + name)
-    })
-    .post('/world', (req, res) => {
-        let name = ""
-        console.log(req.body)
-        if (req.body.name) {
-            name = req.body.name
-        }
-        res.send('Hello World, ' + name)
-    })
+    .get('/', simple)
+    .get('/hello', hello)
+    .post('/world', world)
 
 module.exports = router;
+
+function simple(req, res, next) {
+    return res.send('Simple REST API')
+}
+
+function hello(req, res, next) {
+    let name = ""
+    if (req.query.name) {
+        name = req.query.name
+    }
+    return res.send('Hello World, ' + name)
+}
+
+function world(req, res, next) {
+    let name = ""
+    console.log(req.body)
+    if (req.body.name) {
+        name = req.body.name
+    }
+    return res.send('Hello World, ' + name)
+}
